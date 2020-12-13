73f90709d9be0275f4232d6ea421c9f954c70dda
Macie <msypniewski511@gmail.com>
2020-12-13 21:34:10 +0000

    Add authorizing signed in users

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -17,16 +17,27 @@ const signin = async (req, res) => {
 		return res.status('401').json({ error: 'Could not sign in' });
 	}
 };
+
 const signout = (req, res) => {
 	res.clearCookie('t');
 	return res.status('200').json({
 		message: 'signed out',
 	});
 };
+
 const requireSignin = expressJwt({
 	secret: config.jwtSecret,
 	userProperty: 'auth',
 });
-const hasAuthorization = (req, res) => {};
+
+const hasAuthorization = (req, res, next) => {
+	const authorized = req.profile && req.auth && req.profile._id == req.auth._id;
+	if (!authorized) {
+		return res.status('403').json({
+			error: 'User is not authorized',
+		});
+	}
+	next();
+};
 
 export default { signin, signout, requireSignin, hasAuthorization };
