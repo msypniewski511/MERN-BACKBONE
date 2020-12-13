59c6ac90fcd3d5204d73bf2db2766a05cc4f1edb
Maciej Sypniewski <msypniewski511@gmail.com>
2020-12-13 14:38:24 +0000

    Msypniewski511/issue7/1 (#12)
    * Set up folder structure
    
    * Basic server and express setup
    
    * Add mongoose
    
    * Setting up the skeleton backend
    Closes #7

--- server/server.js
+++ server/server.js
@@ -1,25 +1,17 @@
-import express from 'express';
-import path from 'path';
-import devBundle from './devBundle';
-import template from './../template';
-import { MongoClient } from 'mongodb';
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup';
-MongoClient.connect(url, (err, db) => {
-	console.log('Connected successfully to mongodb server');
-	db.close();
-});
-const CURRENT_WORKING_DIR = process.cwd();
-const app = express();
-devBundle.compile(app);
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
-app.get('/', (req, res) => {
-	res.status(200).send(template());
+import config from './../config/config';
+import app from './express';
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+mongoose.connection.on('error', () => {
+	throw new Error(`unable to connect to database: ${mongoUri}`);
 });
 
-let port = process.env.PORT || 3000;
-app.listen(port, function onStart(err) {
+app.listen(config.port, err => {
 	if (err) {
 		console.log(err);
 	}
-	console.info('Server started on port %s.', port);
+	console.info('Server started on port %s.', config.port);
 });
