5e217770095de2de2d617824ce8d77f1f6cc0e69
Macie <msypniewski511@gmail.com>
2020-12-13 21:42:47 +0000

    Add authorisation and authentication functionality closes #issue8

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -28,6 +28,7 @@ const signout = (req, res) => {
 const requireSignin = expressJwt({
 	secret: config.jwtSecret,
 	userProperty: 'auth',
+	algorithms: ['RS256'],
 });
 
 const hasAuthorization = (req, res, next) => {
