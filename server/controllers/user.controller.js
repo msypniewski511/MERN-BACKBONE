b104fe4ea7b6c2e5369c2d4ec71e1309cbbaa70b
Macie <msypniewski511@gmail.com>
2020-12-13 20:36:11 +0000

    Add delete user method to the controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -61,6 +61,18 @@ const update = async (req, res) => {
 		});
 	}
 };
-const remove = (req, res, next) => {};
+const remove = async (req, res) => {
+	try {
+		let user = req.profile;
+		let deletedUser = await user.remove();
+		deletedUser.hashed_password = undefined;
+		deletedUser.salt = undefined;
+		res.json(deletedUser);
+	} catch (err) {
+		return res.status(400).json({
+			error: errorHandler.getErrorMessage(err),
+		});
+	}
+};
 
 export default { create, userByID, read, list, remove, update };
