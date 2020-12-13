e62faea1f9459ba6cc8acba5242ca003780148fd
Macie <msypniewski511@gmail.com>
2020-12-13 20:33:58 +0000

    Add update user profile method to the controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -46,7 +46,21 @@ const read = (req, res) => {
 	return res.json(req.profile);
 };
 
-const update = (req, res, next) => {};
+const update = async (req, res) => {
+	try {
+		let user = req.profile;
+		user = extend(user, req.body);
+		user.updated = Date.now();
+		await user.save();
+		user.hashed_password = undefined;
+		user.salt = undefined;
+		res.json(user);
+	} catch (err) {
+		return res.status(400).json({
+			error: errorHandler.getErrorMessage(err),
+		});
+	}
+};
 const remove = (req, res, next) => {};
 
 export default { create, userByID, read, list, remove, update };
